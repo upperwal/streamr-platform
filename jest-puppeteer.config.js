3916d7440be466a71e2b38bfa97490e7caa6dbdd
Tommi <mato12@hotmail.fi>
2018-06-04 10:58:19 +0300

    change configs

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,9 +1,7 @@
 module.exports = {
     server: {
         launchTimeout: 99999999,
-        command: `
-            ./node_modules/.bin/webpack;
-            ./node_modules/.bin/http-server -p 3333 dist`,
+        command: './node_modules/.bin/webpack-dev-server',
         port: 3333,
     },
 }
