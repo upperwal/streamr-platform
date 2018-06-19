a6b344fefe2a4e459b512927cf874cc948d7eb21
Tommi <mato12@hotmail.fi>
2018-06-19 15:50:04 +0300

    Add travis script to run e2e

--- travis_scripts/e2e-tests.sh
+++ travis_scripts/e2e-tests.sh
@@ -1,13 +1,10 @@
-sudo /etc/init.d/mysql stop
+# sudo /etc/init.d/mysql stop
 git clone https://github.com/streamr-dev/streamr-docker-dev.git
 $TRAVIS_BUILD_DIR/streamr-docker-dev/streamr-docker-dev/bin.sh start 1
-sleep(1000)
+sleep 1000
 $TRAVIS_BUILD_DIR/streamr-docker-dev/streamr-docker-dev/bin.sh engine-and-editor
 
-while ()
-do
-
-done
+bash ./utils/wait-for-it.sh localhost:8081 -- echo "Engine and editor is up and running!"
 
 NODE_ENV=e2e npm run build
 npm run test-e2e
