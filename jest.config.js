427e429554227f2c1cd3efbde57f104a2a9b1b2b
Aapeli <aapeli.haanpuu@streamr.com>
2018-06-21 15:48:04 +0300

    Jest config away from package json

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,14 @@ module.exports = {
     collectCoverage: true,
     coverageThreshold: {
         global: {
-            lines: 24,
+            lines: 33,
         },
     },
     collectCoverageFrom: [
         'src/**/*.{js,jsx}',
     ],
+    moduleNameMapper: {
+        '\\.(css|scss|pcss)$': 'empty/object',
+    },
+    setupTestFrameworkScriptFile: '<rootDir>/test/test-utils/setupTests.js',
 }
