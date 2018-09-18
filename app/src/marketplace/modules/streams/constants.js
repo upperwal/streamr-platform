b2b261117c1eabe3848b578efa5cb0657b89337d
fonty1 <matthew.fontana@streamr.com>
2018-09-18 14:42:28 +0300

    Remove Marketplace inside consolidated App

--- app/src/marketplace/modules/streams/constants.js
+++
@@ -1,5 +0,0 @@
-// @flow
-
-export const GET_STREAMS_REQUEST: string = 'marketplace/streams/GET_STREAMS_REQUEST'
-export const GET_STREAMS_SUCCESS: string = 'marketplace/streams/GET_STREAMS_SUCCESS'
-export const GET_STREAMS_FAILURE: string = 'marketplace/streams/GET_STREAMS_FAILURE'
