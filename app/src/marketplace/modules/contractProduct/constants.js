69f8a526e86f59058f8c05ef5225451d4462a032
fonty1 <matthew.fontana@streamr.com>
2018-09-18 14:54:11 +0300

    delete app/marketplace

--- app/src/marketplace/modules/contractProduct/constants.js
+++
@@ -1,6 +0,0 @@
-// @flow
-
-export const GET_PRODUCT_FROM_CONTRACT_REQUEST: string = 'marketplace/contractProduct/GET_PRODUCT_FROM_CONTRACT_REQUEST'
-export const GET_PRODUCT_FROM_CONTRACT_SUCCESS: string = 'marketplace/contractProduct/GET_PRODUCT_FROM_CONTRACT_SUCCESS'
-export const GET_PRODUCT_FROM_CONTRACT_FAILURE: string = 'marketplace/contractProduct/GET_PRODUCT_FROM_CONTRACT_FAILURE'
-export const CLEAR_CONTRACT_PRODUCT: string = 'marketplace/contractProduct/CLEAR_CONTRACT_PRODUCT'
